b623a9d99f1d3f8334236679866da34c6de4fe64
Maarouf Yassine <maarouf_yassine@hotmail.com>
2021-11-24 20:59:42 +0200

    feat: display video from selected source

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const videoElement = document.querySelector('video');
 const videoSelect = document.querySelector('select#videoSource');
 const selectors = [videoSelect];
+const canvas = document.getElementById('canvasOutput');
 
 function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
   const values = selectors.map(select => select.value);
   selectors.forEach(select => {
     while (select.firstChild) {
@@ -39,15 +41,6 @@ let vc = null;
 let width = 0;
 let height=0;
 
-// let lower_green = [40,70,80];
-// let upper_green = [70,255,255];
-
-// let lower_red = [0,50,120];
-// let upper_red = [10,255,255];
-
-// let lower_blue = [90,60,0];
-// let upper_blue = [121,255,255];
-
 
 function start() {
   if (window.stream) {
@@ -77,54 +70,11 @@ function start() {
       videoElement.setAttribute("width", width);
       videoElement.setAttribute("height", height);
       streaming = true;
+      vc = new cv.VideoCapture(videoElement);
     }
   }, false);
 }
 
-
 videoSelect.onchange = start;
 
 start();
-
-
-window.onload = function() {
-  var canvas = document.getElementById('canvas');
-  canvas.width=640;
-  canvas.height=480;
-  var context = canvas.getContext('2d');
-
-  tracking.ColorTracker.registerColor('purple', function(r, g, b) {
-    var dx = r - 120;
-    var dy = g - 60;
-    var dz = b - 210;
-
-    if ((b - g) >= 100 && (r - g) >= 60) {
-      return true;
-    }
-    return dx * dx + dy * dy + dz * dz < 3500;
-  });
-
-  var tracker = new tracking.ColorTracker(['yellow', 'purple']);
-  tracker.setMinDimension(5);
-
-  tracking.track('#video', tracker);
-
-  tracker.on('track', function(event) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    event.data.forEach(function(rect) {
-      if (rect.color === 'custom') {
-        rect.color = tracker.customColor;
-      }
-
-      context.strokeStyle = rect.color;
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-    });
-  });
-
-  initGUIControllers(tracker);
-};
